9b23c1d35f10bf68f0405eff653ef28797c3b8bd
Jeff Forbes <jforbes@dropbox.com>
2021-04-05 20:23:05 -0400

    Remove Pods.

--- Pods/Target Support Files/MinervaCoordinator/MinervaCoordinator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MinervaCoordinator : NSObject
-@end
-@implementation PodsDummy_MinervaCoordinator
-@end
--- Pods/Target Support Files/MinervaList/MinervaList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MinervaList : NSObject
-@end
-@implementation PodsDummy_MinervaList
-@end
